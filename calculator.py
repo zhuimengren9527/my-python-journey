685c9f26a2e225370ffb198d28c1da05fb3464dd
仁亚东 <277812274@qq.com>
2025-10-17 17:59:57 +0800

    完成第二天练习：计算器

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# 获取用户输入
+num1 = float(input("请输入第一个数字: "))
+num2 = float(input("请输入第二个数字: "))
+
+# 计算
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+# 输出结果
+print("加法结果:", add)
+print("减法结果:", sub)
+print("乘法结果:", mul)
+print("除法结果:", div)
