88c01297a3bca7f128c75e8ec8c4ed6a2a1ddf0a
仁亚东 <277812274@qq.com>
2025-10-18 18:38:00 +0800

    sort管理方法

---
+++ manage.py
@@ -0,0 +1,21 @@
+cars=['bmw','audi','toyota','subaru']
+cars.sort() # 按字母顺序排列
+print(cars,"\n")
+
+cars=['bmw','audi','toyota','subaru']
+cars.sort(reverse=True)
+print(cars,"\n")
+
+cars=['bmw','audi','toyota','subaru']
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars,"\n")
+
+cars=['bmw','audi','toyota','subaru']
+print(f"这个列表中有{len(cars)} 个元素")
+
